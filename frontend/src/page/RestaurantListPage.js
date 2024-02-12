b0e2b43d7fd3b69f395b7530253deb28895bb436
PuNe <57552176+worachanon226@users.noreply.github.com>
2024-02-12 18:43:01 +0700

    Update RestaurantListPage.js

--- frontend/src/page/RestaurantListPage.js
+++ frontend/src/page/RestaurantListPage.js
@@ -2,18 +2,21 @@ import React from "react";
 import axios from "axios";
 import './RestaurantListPage.css'
 
-function RestaurantListPage(props)
-{
+function RestaurantListPage(props) {
     const url = "http://localhost:3001/member/restaurant";
+    const accessToken = localStorage.getItem("token")
 
-    axios.get(url).then((response) => 
-    {
+    axios.get(url, {
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        }
+    }).then((response) => {
         // handle success
         console.log(response);
     })
-    .catch((error) => {
-        console.log(error)
-    });    
+        .catch((error) => {
+            console.log(error)
+        });
 
     return (
         <p>xxxx</p>
