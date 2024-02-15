7619a626350199a8d116d947281436f4fa46dffe
pxxptm <ppp.pattamon@gmail.com>
2024-02-15 14:28:16 +0700

    update restaurant card

--- frontend/src/component/RestaurantCard.js
+++ frontend/src/component/RestaurantCard.js
@@ -2,18 +2,24 @@ import React from 'react'
 import './RestaurantCard.css'
 
 function RestaurantCard(props) {
+  const url = "http://localhost:3000/" + props.restaurantName
   return (
-    <div id="rest-card-container">
-        <div id="rest-card-pic"></div>
-        <div id="rest-card-content">
-            <div id="rest-card-name">
-                {props.restaurantName}
-            </div>
-            <div id="rest-card-description">
-                {props.restaurantDescription}
-            </div>
-        </div>
-    </div>
+    <a id="rest-card-link" href={url}>
+      <div id="rest-card-container">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
+          <div id="rest-card-pic">
+            <button><i className="material-icons">border_color</i></button>
+          </div>
+          <div id="rest-card-content">
+              <div id="rest-card-name">
+                  {props.restaurantName}
+              </div>
+              <div id="rest-card-description">
+                  {props.restaurantDescription}
+              </div>
+          </div>
+      </div>
+    </a>
   )
 }
 
