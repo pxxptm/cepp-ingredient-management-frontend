13141e1ec80ed85b820c8ff3eba7f93f12b6ae97
PuNe <57552176+worachanon226@users.noreply.github.com>
2024-02-24 09:00:30 +0700

    update: updateIngredient

--- backend/src/modules/ingredient/ingredient.controller.ts
+++ backend/src/modules/ingredient/ingredient.controller.ts
@@ -3,11 +3,12 @@ import {
   Controller,
   Delete,
   Param,
+  Patch,
   Post,
   UseGuards,
 } from '@nestjs/common';
 import { IngredientService } from './ingredient.service';
-import { CreateIngredientDto } from './dto/ingredient.dto';
+import { CreateIngredientDto, UpdateIngredientDto } from './dto/ingredient.dto';
 import { AuthGuard } from '../auth/guard/auth.guard';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
@@ -21,6 +22,14 @@ export class IngredientController {
   @Post()
   async create(@Body() createIngredientDto: CreateIngredientDto) {
     return await this.ingredientService.create(createIngredientDto);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateIngredientDto: UpdateIngredientDto,
+  ) {
+    return await this.ingredientService.update(id, updateIngredientDto);
   }
 
   @Delete(':id')
